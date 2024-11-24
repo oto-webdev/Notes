29226a9082280dbdafdb6d23e17e77897888740f
oto-webdev <r.otowebdev@gmail.com>
2024-11-24 21:42:13 +0400

    fetch using axios

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import axios from 'axios'
 
 const App = () => {
+
+  const [notes, setNotes] = useState([])
+
+  const fetchNotes = async () => {
+    const res = await axios.get("http://localhost:4000/api/notes")
+
+    setNotes(res.data.notes)
+  }
+
+  useEffect(() => {
+    fetchNotes()
+  }, [])
+
   return (
-    <div className=''>
+    <div>
+      <div className='app'>
+      <h2>Notes:</h2>
+      {notes && notes.map(note => {
+        return <div key={note._id}>
+          <h3>{note.title}</h3>
+        </div>
+      })}
+    </div>
+
+    <form>
       
+    </form>
     </div>
   )
 }
